7b74c12999d4a40a405a594f3c14dc9d93fdc800
ymori <yuu.mori06@gmail.com>
2022-10-07 18:38:11 +0900

    update: add const

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -9,6 +9,16 @@
 
 template<typename T>
 void iter(T *arr, std::size_t arrLen, void (func)(T &)) {
+  if (!arr || !func)
+    return ;
+  for (std::size_t i = 0; i < arrLen; i++)
+    func(arr[i]);
+}
+
+template<typename T>
+void iter(const T *arr, std::size_t arrLen, void (func)(const T &)) {
+  if (!arr || !func)
+    return ;
   for (std::size_t i = 0; i < arrLen; i++)
     func(arr[i]);
 }
